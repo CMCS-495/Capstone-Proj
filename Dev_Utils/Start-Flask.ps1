99c1fedcd8bd40f11a1764aae3a2a7c5f4121e96
orwiad10 <orwiad10@gmail.com>
2025-07-21 19:54:47 -0400

    Add process management for Flask server startup in Start-Flask.ps1

--- Dev_Utils/Start-Flask.ps1
+++ Dev_Utils/Start-Flask.ps1
@@ -1,3 +1,13 @@
+$flaskpid = try{
+    Get-NetTCPConnection -LocalPort 5000 -State Listen -ErrorAction Stop | Select-Object -ExpandProperty OwningProcess
+}catch{
+    $null
+}
+
+if($flaskpid){
+    Get-Process -Id $flaskpid | Stop-Process -Force -Confirm:$false
+}
+
 $scriptDir   = Split-Path -Parent $MyInvocation.MyCommand.Path
 $projectRoot = Resolve-Path "$scriptDir\.."
 Push-Location $projectRoot
