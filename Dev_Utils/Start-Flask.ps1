6655241bd627b26374420e1a4a4658c6bae10d1e
orwiad10 <orwiad10@gmail.com>
2025-07-24 16:03:10 -0400

    Refactor Start-Flask.ps1 to improve Flask startup process and ensure proper handling of existing instances

--- Dev_Utils/Start-Flask.ps1
+++ Dev_Utils/Start-Flask.ps1
@@ -1,3 +1,4 @@
+#check if Flask is already running and stop it if necessary
 $flaskpid = try{
     Get-NetTCPConnection -LocalPort 5000 -State Listen -ErrorAction Stop | Select-Object -ExpandProperty OwningProcess
 }catch{
@@ -8,12 +9,21 @@ if($flaskpid){
     Get-Process -Id $flaskpid | Stop-Process -Force -Confirm:$false
 }
 
+#build flask path
 $scriptDir   = Split-Path -Parent $MyInvocation.MyCommand.Path
 $projectRoot = Resolve-Path "$scriptDir\.."
 Push-Location $projectRoot
 $env:FLASK_APP = 'Flask.flask_app'
 
+#start flask as a subprocess
+Start-Process -NoNewWindow -FilePath "flask" -ArgumentList "run"
+
+#wait for Flask to start
+while(-not (Get-NetTCPConnection -LocalPort 5000 -State Listen -ErrorAction SilentlyContinue)) {
+    Start-Sleep -Milliseconds 500
+}
+
+#open the browser to the Flask app
 Start-Process "http://127.0.0.1:5000/"
-flask run
 
 Pop-Location
